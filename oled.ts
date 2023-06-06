f2255a5dd925be33fe85c6b915147c2823450cee
lbaitemple <lbai@temple.edu>
2023-06-06 08:40:03 -0400

    Update oled.ts

--- oled.ts
+++ oled.ts
@@ -77,8 +77,8 @@ namespace zumo {
      */
     //% blockId=createI2C
     //% block="create I2C at scl $scl and sda $sda "
-    //% scl.defl=blocks.fieldPin("PIN_SCL")
-    //% sda.defl=blocks.fieldPin("PIN_SDA")
+    //% scl.defl=blocks.fieldPin("SCL")
+    //% sda.defl=blocks.fieldPin("SDA")
     //% subcategory=SSD1306
     export function createI2C(scl: DigitalInOutPin, sda: DigitalInOutPin): void {
         _i2c = pins.createI2C(sda, scl);
