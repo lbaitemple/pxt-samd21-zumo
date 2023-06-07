c5dc5d22a82a886c860095888834bb8a353eeea7
lbaitemple <lbai@temple.edu>
2023-06-06 22:55:16 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,20 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+input.buttonD12.onEvent(ButtonEvent.Click, function () {
+    pause(500)
+    while (!(input.buttonD12.isPressed())) {
+        zumo.clear()
+        zumo.writeStringNewLine(zumo.readString(zumo.calibratedMaximumOn_values()))
+        zumo.writeStringNewLine(zumo.readString(zumo.calibratedMinimumOn_values()))
+        sensors = zumo.readLine()
+        zumo.writeNumNewLine(zumo.getLineNumber())
+        zumo.writeStringNewLine(zumo.readString(sensors))
+        pause(1000)
+    }
+})
+let sensors: number[] = []
+zumo.createI2C(pins.SCL, pins.SDA)
+zumo.init(128, 64)
+zumo.enableIMU()
+zumo.Initialization()
+zumo.writeStringNewLine("ready")
+zumo.writeStringNewLine(zumo.Showerr())
