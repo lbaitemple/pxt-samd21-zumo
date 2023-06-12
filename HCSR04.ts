9cec094a56dd20e9594b946ef06caf2e385b1be4
lbaitemple <lbai@temple.edu>
2023-06-12 13:54:33 -0400

    Update HCSR04.ts

--- HCSR04.ts
+++ HCSR04.ts
@@ -1,1 +1,28 @@
 // Add your code here
+
+namespace zumo {
+    let echoPin: DigitalInOutPin = pins.D2;
+    let trigPin: DigitalInOutPin= pins.D7;
+
+    function calculateDistance(): number {
+        trigPin.digitalWrite(false);
+        pause(2);
+        trigPin.digitalWrite(true);
+        pause(10);
+        trigPin.digitalWrite(false);
+        
+        let d = echoPin.pulseIn(PulseValue.High, 23529.4);
+
+        return d / 58.8235;
+    }
+
+    //% block="Ultrasound at trig $trig and echo $echo "
+    //% trig.defl=pins.SCL
+    //% echo.defl=pins.SDA
+    //% subcategory=Ultrasound
+    export function HCSR04(trig:DigitalInOutPin, echo:DigitalInOutPin): number{
+        echoPin = echo;
+        trigPin = trig;
+        return calculateDistance();
+    }
+}
