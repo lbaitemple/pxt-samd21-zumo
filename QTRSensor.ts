e6686e1f022ec672452b8a98e803c9d6b5132fae
lbaitemple <lbai@temple.edu>
2023-06-12 18:27:10 -0400

    Update QTRSensor.ts

--- QTRSensor.ts
+++ QTRSensor.ts
@@ -251,7 +251,7 @@ namespace zumo {
             _err1 = sensor_values[_numSensors - 1].toString() + ":" + _maxValue.toString();
             let time = control.micros() - startTime;
             for (i = 0; i < _numSensors; i++) {
-                if (_pins[i].digitalRead() === false && time < sensor_values[i])
+                if (_pins[i].digitalRead() === false) // && time < sensor_values[i])
                     sensor_values[i] = time;
             }
             _err = time.toString() + " " + sensor_values[_numSensors-1].toString();
