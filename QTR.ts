3d1985c7b4153354d3d18a97e3236297f41dfabf
lbaitemple <lbai@temple.edu>
2023-06-13 20:55:52 -0400

    Update QTR.ts

--- QTR.ts
+++ QTR.ts
@@ -1,29 +1,41 @@
 // Add your code here
 namespace zumo {
-    let color: number=1, last_color: number=1, last:number=0;
-    //% block="QTR at $pin with Emit $epin "
-    //% pin.defl=pins.D2
-    //% epin.defl=pins.D4
+    let pinValues: boolean[] = [];
+    let eventTimes: number[] = [];
+
+
+    //% block="QTR using Emit $epin "
+    //% epin.defl=pins.D2
     //% subcategory=QTR
-    export function QTR(pin: DigitalInOutPin, epin: DigitalInOutPin): number {
+    export function QTR(epin: DigitalInOutPin): number [] {
         epin.digitalWrite(true);
-        pin.digitalWrite(true)
         control.waitMicros(12);
-        pin.digitalWrite(false)
-        let time:number = control.micros();
-        
-        
-        while (pin.digitalRead() == true) { }
-        
-        if ((control.micros() - time) > last && color == 1) {
-            color = -1;
-        } else if ((control.micros() - time) < last && color == -1) {
-            color = 1;
-        }
+        readPins();
         epin.digitalWrite(false);
-        last = control.micros() - time;
-        last_color = color;
-        return color;
+
+        return eventTimes;
+    }
+
+
+
+    function readPins(): void {
+        const pinsToRead: DigitalInOutPin[] = [pins.D4, pins.A3, pins.D11, pins.A0, pins.A2, pins.D5];
+
+        control.runInParallel(() => {
+            for (let i = 0; i < pinsToRead.length; i++) {
+                const pin = pinsToRead[i];
+                const startTime = control.millis();
+                const value = pin.digitalRead();
+                const eventTime = control.millis() - startTime;
+                storePinValueAndTime(i, value, eventTime);
+            }
+        });
+    }
+
+    function storePinValueAndTime(index: number, value: boolean, time: number): void {
+        // Store the pin value and event time in the respective arrays
+        pinValues[index] = value;
+        eventTimes[index] = time;
     }
 
 }
