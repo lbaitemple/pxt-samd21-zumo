f842c934a09acd3352cd0dc6d4bdd51da17a865b
lbaitemple <lbai@temple.edu>
2023-06-14 15:36:29 -0400

    Update QTRSensor.ts

--- QTRSensor.ts
+++ QTRSensor.ts
@@ -112,7 +112,7 @@ namespace zumo {
         for (i = 0; i < _numSensors; i++) {
             let value = sensor_values[i];
             if (whiteLine)
-                value = 1000 - value;
+                value = value -1000;
 
             // keep track of whether we see the line at all
             if (value > 200) {
