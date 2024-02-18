e868895472fe726ade429991440ceadf08bf26cd
Li Bai <lbai@temple.edu>
2024-02-18 12:45:49 -0500

    Update zumo.ts

--- zumo.ts
+++ zumo.ts
@@ -219,6 +219,8 @@ namespace zumo {
             leftMotorstate = ZumoMotors.LEFT_OFF;
             rightMotorstate = ZumoMotors.RIGHT_OFF;
         }
+        control.waitMicros(500000); // wait until the state is updated.
+        
     }
 
 
