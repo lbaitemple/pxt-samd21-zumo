24c4b2cc1ed409d7156a1f1da1a0cbc8f56b33ce
Li Bai <lbai@temple.edu>
2024-02-18 10:16:37 -0500

    Update zumo.ts

--- zumo.ts
+++ zumo.ts
@@ -101,7 +101,9 @@ namespace zumo {
             if (speed === 100 || speed === -100) {
                 // Avoid PWM whenever possible as only 3 concurrent PWM outputs are available on the microbit
                 //pins.digitalWritePin(DigitalPin.P13, 1);
-                pins.D9.digitalWrite(true);
+                if (pins.D9.digitalRead()==false){
+                    pins.D9.digitalWrite(true);
+                }
 
             } else {
                 pins.D9.analogSetPeriod(255);
@@ -114,7 +116,9 @@ namespace zumo {
             if (speed === 100 || speed === -100) {
                 // Avoid PWM whenever possible as only 3 concurrent PWM outputs are available on the microbit
                 //pins.digitalWritePin(DigitalPin.P14, 1);
-                pins.D10.digitalWrite(true);
+                if (pins.D10.digitalRead() == false) {
+                    pins.D10.digitalWrite(true);
+                }
             } else {
                 pins.D10.analogSetPeriod(255);
                 pins.D10.analogWrite(analogSpeed);
@@ -128,8 +132,12 @@ namespace zumo {
             if (speed === 100 || speed === -100) {
                 // Avoid PWM whenever possible as only 3 concurrent PWM outputs are available on the microbit
                 //pins.digitalWritePin(DigitalPin.P13, 1);
-                pins.D10.digitalWrite(true);
-                pins.D9.digitalWrite(true);
+                if (pins.D10.digitalRead() == false) {
+                    pins.D10.digitalWrite(true);
+                }
+                if (pins.D9.digitalRead() == false) {
+                    pins.D9.digitalWrite(true);
+                }
             } else {
                 pins.D10.analogSetPeriod(255);
                 pins.D10.analogWrite(analogSpeed);
