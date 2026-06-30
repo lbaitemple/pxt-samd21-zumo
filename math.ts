d7f8f9d1ae8b9fb6fe162475b15080745739ce8e
Li Bai <lbai@temple.edu>
2026-06-30 15:24:38 -0400

    Update math.ts

--- math.ts
+++ math.ts
@@ -3,15 +3,17 @@ namespace zumo {
 function toRadians(deg: number): number {
     return deg * Math.PI / 180
 }
-//% block="cos of %angle"
-//% angle.defl=0
+//% block="cos of %deg"
+//% deg.defl=0
+ //% subcategory=Math
 function toCosine(deg: number):number{
     return Math.cos(toRadians(deg))  // cos(45°)
 }
 
 
-//% block="sin of %angle"
-//% angle.defl=0
+//% block="sin of %deg"
+//% deg.defl=0
+ //% subcategory=Math
 function toSine(deg: number): number {
     return Math.sin(toRadians(deg))  // cos(45°)
 }
